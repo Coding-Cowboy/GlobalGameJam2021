e22983167649325f09da4756cc1e70ae5474f748
Garrett Wagner <Garrett.Wagner@eagles.oc.edu>
2021-01-29 18:12:24 -0600

    Got Bullets to fire from Gun Barrel

--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class ShootingScript : MonoBehaviour
 {
-    public GameObject[] Bullets = new GameObject[4];//List of bullets for if we need more of a spread
     
-    public float BulletOffset;//This is the size of the offest between bullets
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +16,11 @@ public class ShootingScript : MonoBehaviour
     {
         
     }
-    public void Fire(GameObject BulletPrefab, Vector3 StartPosition, Vector3 Rotation, float BulletSpeed)
+    public void fire(Vector3 AdditionalRotation, Vector3 Force)
     {
-        //Instantiate(BulletPrefab);
-        transform.eulerAngles = Rotation;
-        //Add Force
-        GetComponent<Rigidbody>().AddForce(Bullets[0].transform.forward * BulletSpeed);
+        //Add to the Rotation
+        transform.eulerAngles += AdditionalRotation;
+        GetComponent<Rigidbody>().AddForce(transform.forward);
+        Debug.Log("Firing");
     }
 }
