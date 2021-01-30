38b14b60b27e8418de7621ef66aafc7791d11cd3
Lane Simpson <lsimpson1040@gmail.com>
2021-01-30 12:10:27 -0600

    End of game trigger

---
+++ Assets/Scripts/EndOfGameTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfGameTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger enter");
+        if (other.tag == "Player")
+        {
+            Debug.Log("End of game sequence");
+        }
+    }
+}
